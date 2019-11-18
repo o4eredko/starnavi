de1a83c1e9a8387ce6b883cac284daff9502bea1
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-18 17:14:23 +0200

    Tests for login

--- blog/tests.py
+++ blog/tests.py
@@ -51,3 +51,29 @@ class RegistrationTestCase(APITestCase):
 		data.pop('email')
 		response = self.client.post(self.registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class TokenLoginTestCase(APITestCase):
+	data = {
+		'username': 'test_username',
+		'email': 'test_email@gmail.com',
+		'password': 'SecretPassword123',
+		'password2': 'SecretPassword123'
+	}
+	registration_url = reverse('account-register')
+	login_url = reverse('account-token')
+
+	def register_user(self):
+		self.client.post(self.registration_url, self.data)
+
+	def test_login_valid(self):
+		self.register_user()
+		login_data = {'username': self.data['username'], 'password': self.data['password']}
+		response = self.client.post(self.login_url, login_data)
+		self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+	def test_login_invalid(self):
+		self.register_user()
+		login_data = {'username': self.data['username'], 'password': '123'}
+		response = self.client.post(self.login_url, login_data)
+		self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
