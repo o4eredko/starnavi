06b33cedefd08dd43cbb0c082c6f00e3e6826d1b
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-18 17:26:56 +0200

    Added Refresh token test

--- blog/tests.py
+++ blog/tests.py
@@ -3,9 +3,12 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+registration_url = reverse('account-register')
+login_url = reverse('account-token')
+refresh_url = reverse('account-refresh')
+
 
 class RegistrationTestCase(APITestCase):
-	registration_url = reverse('account-register')
 	data = {
 		'username': 'test_username',
 		'email': 'test_email@gmail.com',
@@ -14,42 +17,42 @@ class RegistrationTestCase(APITestCase):
 	}
 
 	def test_registration_valid(self):
-		response = self.client.post(self.registration_url, self.data)
+		response = self.client.post(registration_url, self.data)
 		self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
 	def test_registration_invalid_passwords(self):
 		data = dict(self.data)
 		data['password'] = data['password2'] = '123'
-		response = self.client.post(self.registration_url, data)
+		response = self.client.post(registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 		# passwords not equal
 		data['password'] = self.data['password']
-		response = self.client.post(self.registration_url, data)
+		response = self.client.post(registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 	def test_registration_data_exists(self):
 		data = dict(self.data)
-		self.client.post(self.registration_url, data)
+		self.client.post(registration_url, data)
 
 		# check if username exists
-		response = self.client.post(self.registration_url, data)
+		response = self.client.post(registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 		# check if email exists
 		data['username'] = 'test_username2'
-		response = self.client.post(self.registration_url, data)
+		response = self.client.post(registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 	def test_registration_data_missing(self):
 		data = dict(self.data)
 		data.pop('username')
-		response = self.client.post(self.registration_url, data)
+		response = self.client.post(registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 		data = dict(self.data)
 		data.pop('email')
-		response = self.client.post(self.registration_url, data)
+		response = self.client.post(registration_url, data)
 		self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
@@ -60,20 +63,43 @@ class TokenLoginTestCase(APITestCase):
 		'password': 'SecretPassword123',
 		'password2': 'SecretPassword123'
 	}
-	registration_url = reverse('account-register')
-	login_url = reverse('account-token')
 
-	def register_user(self):
-		self.client.post(self.registration_url, self.data)
-
-	def test_login_valid(self):
-		self.register_user()
-		login_data = {'username': self.data['username'], 'password': self.data['password']}
-		response = self.client.post(self.login_url, login_data)
-		self.assertEquals(response.status_code, status.HTTP_200_OK)
+	def setUp(self):
+		self.client.post(registration_url, self.data)
 
 	def test_login_invalid(self):
-		self.register_user()
 		login_data = {'username': self.data['username'], 'password': '123'}
-		response = self.client.post(self.login_url, login_data)
+		response = self.client.post(login_url, login_data)
 		self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_login_valid(self):
+		login_data = {'username': self.data['username'], 'password': self.data['password']}
+		response = self.client.post(login_url, login_data)
+		self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+
+class TokenRefreshTestCase(APITestCase):
+	data = {
+		'username': 'test_username',
+		'email': 'test_email@gmail.com',
+		'password': 'SecretPassword123',
+		'password2': 'SecretPassword123'
+	}
+
+	def setUp(self):
+		self.client.post(registration_url, self.data)
+		login_data = {'username': self.data['username'], 'password': self.data['password']}
+		response = self.client.post(login_url, login_data)
+		self.access_token = response.data['access']
+		self.refresh_token = response.data['refresh']
+
+	def test_token_refresh_invalid(self):
+		refresh_data = {'refresh': '123'}
+		response = self.client.post(refresh_url, refresh_data)
+		self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_token_refresh_valid(self):
+		refresh_data = {'refresh': self.refresh_token}
+		response = self.client.post(refresh_url, refresh_data)
+		self.access_token = response.data['access']
+		self.assertEquals(response.status_code, status.HTTP_200_OK)
