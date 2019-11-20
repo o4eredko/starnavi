131cf7e6a4695a5d0f48bfd30df89d2f0845d0a7
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-20 20:49:27 +0200

    Fixed bug with permissions for like

--- blog/mixins.py
+++ blog/mixins.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from . import services
@@ -6,17 +7,19 @@ from .serializers import UserSerializer
 
 
 class LikedMixin:
-	@action(detail=True, methods=['GET', 'POST'])
+	@action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
 	def like(self, request, pk=None):
 		obj = self.get_object()
+		serializer = self.get_serializer(obj)
 		services.add_like(obj, request.user)
-		return Response()
+		return Response(serializer.data)
 
-	@action(detail=True, methods=['GET', 'POST'])
+	@action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
 	def unlike(self, request, pk=None):
 		obj = self.get_object()
+		serializer = self.get_serializer(obj)
 		services.remove_like(obj, request.user)
-		return Response()
+		return Response(serializer.data)
 
 	@action(detail=True, methods=['GET'])
 	def fans(self, request, pk=None):
